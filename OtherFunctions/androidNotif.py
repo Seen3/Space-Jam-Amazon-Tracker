7e59236eb87eee1698f313b9dd64cecf61a2dee7
Md Taseen Atehar <63313585+Seen3@users.noreply.github.com>
2021-02-27 16:06:08 +0530

    Update androidNotif.py

--- OtherFunctions/androidNotif.py
+++ OtherFunctions/androidNotif.py
@@ -3,4 +3,7 @@ notify=Notify()
 def buyNow():
   notify.send("Buy now")
 def buy(title,price,link):
-  notify.send("{} at {} on {}".format(title,price,link))
+  notify.send("""
+The price of the product "{}" has gone below the threshold you have entered to the price Rs.{}.
+Here is the link {}
+""".format(title,price,link))
