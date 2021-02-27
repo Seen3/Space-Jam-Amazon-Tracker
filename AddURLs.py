63e7f2ea2b96893b3803b06f97de06958d5430f2
Batflash2 <69955149+Batflash2@users.noreply.github.com>
2021-02-27 11:38:33 +0530

    AddURLs.py used to add product urls to the database for price checks.

---
+++ AddURLs.py
@@ -0,0 +1,6 @@
+from OtherFunctions.SQL_Functions import Database
+
+db = Database()
+db.get_product_params()
+
+# TODO convert the add url file to url manager to allow the addition as well as deletion of urls
